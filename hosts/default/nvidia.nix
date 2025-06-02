f2895ca12c519637ca506d41cc71462ce8ec85ec
som-04 <somyasati14@gmail.com>
2025-06-03 02:39:00 +0530

    first

---
+++ hosts/default/nvidia.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+
+  environment.systemPackages =
+    let
+      nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      '';
+    in
+    [ nvidia-offload ];
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        offload.enable = true;
+
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+    graphics.enable = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+}
