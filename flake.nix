b933199ed1b12ef3da7646b24238362dd951e06d
som-04 <somyasati14@gmail.com>
2025-08-13 15:04:49 +0300

    added lanzaboote

--- flake.nix
+++ flake.nix
@@ -8,9 +8,16 @@
        url = "github:nix-community/home-manager";
        inputs.nixpkgs.follows = "nixpkgs";
      };
+     lanzaboote = {
+           url = "github:nix-community/lanzaboote/v0.4.2";
+
+           # Optional but recommended to limit the size of your system closure.
+           inputs.nixpkgs.follows = "nixpkgs";
+         };
+
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, lanzaboote,... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -18,6 +25,25 @@
       modules = [
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
+        lanzaboote.nixosModules.lanzaboote
+        ({ pkgs, lib, ... }: {
+
+                    environment.systemPackages = [
+                      # For debugging and troubleshooting Secure Boot.
+                      pkgs.sbctl
+                    ];
+
+                    # Lanzaboote currently replaces the systemd-boot module.
+                    # This setting is usually set to true in configuration.nix
+                    # generated at installation time. So we force it to false
+                    # for now.
+                    boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                    boot.lanzaboote = {
+                      enable = true;
+                      pkiBundle = "/var/lib/sbctl";
+                    };
+                  })
       ];
     };
   };
