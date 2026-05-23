cb2c817bf3acd66fdbf16ad705806eed5bba01df
somya <somyasati14@gmail.com>
2026-05-23 19:59:11 +0300

    fix: micro-stoppages to audio

--- hosts/som/configuration.nix
+++ hosts/som/configuration.nix
@@ -30,6 +30,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 2048;
+          "default.clock.min-quantum" = 2048;
+          "default.clock.max-quantum" = 8192;
+        };
+    };
   };
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
